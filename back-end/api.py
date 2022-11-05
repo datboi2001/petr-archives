5867cf285518196d97dc1f599c7054e4b7c41000
Ella Chung <ellalc@uci.edu>
2022-11-05 13:08:59 -0700

    api functions

--- back-end/api.py
+++ back-end/api.py
@@ -1,6 +1,20 @@
 import json
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI()
 
-@
+@app.get("/")
+def home():
+    return {}
+
+@app.get("/get-data")
+def data():
+    file = open('data.json')
+    JSONdata = json.load(file)
+    file.close()
+    return JSONdata
+
+if __name__ == '__main__':
+    uvicorn.run('api:app', reload=True)
